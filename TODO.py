8a5d1697e4c31c38edca56748a378815a638cb8e
TazMatic <31835653+TazMatic@users.noreply.github.com>
2019-10-15 20:43:15 -0400

    Update TODO.py

--- TODO.py
+++ TODO.py
@@ -1,9 +1,18 @@
 import sys
 import os
-import glob2
 import gitlab
-# TODO add multi language support
-# TODO exclude todo.py from search
+# TODO setup a test to make sure everything is working
+# TODO add support for multi-line comments
+accepted_file_types = {'.py': ['#todo', '# todo'],
+                       '.c': ['//todo', '// todo'],
+                       '.cpp': ['//todo', '// todo'],
+                       '.js': ['//todo', '// todo', '// fixme', '//fixme'],
+                       '.java': ['//todo', '// todo', '// fixme', '//fixme'],
+                       '.php': ['//todo', '// todo', '// fixme', '//fixme'],
+                       '.swift': ['//todo', '// todo', '// fixme', '//fixme'],
+                       '.cs': ['//todo', '// todo'],
+                       '.kt': ['fun todo', '//todo', '// todo'],
+                       '.go': ['//todo', '// todo', '// fixme', '//fixme']}
 
 
 class GitConnect:
@@ -45,17 +54,18 @@ class GitConnect:
 
 
 class FileToScrape:
-    def __init__(self, path):
+    def __init__(self, name, path):
         self.path = path
-        self.name = self.path.split("/")[-1]
+        self.name = name
         self.lines = []
 
-    def read(self):
+    def read(self, file_type):
         try:
             with open(self.path, "r") as f:
                 for line_number, todo_line in enumerate(f):
-                    if "TODO" in todo_line:
-                        self.lines.append("".join(["Line number:", str(line_number), " ",  todo_line.strip()]))
+                    for todo_style in accepted_file_types.get(file_type):
+                        if todo_style in todo_line.lower():
+                            self.lines.append("".join(["Line number:", str(line_number+1), " ",  todo_line.strip()]))
         except Exception as e:
             print(e)
 
@@ -67,12 +77,14 @@ if __name__ == "__main__":
     except ValueError as error:
         print(error)
         sys.exit(1)
-
-    for file in glob2.glob("./**/*.py"):
-        x = FileToScrape(file)
-        if x.name not in sys.argv:
-            files[x.name] = x
-            x.read()
-            for line in x.lines:
-                gl.create_issue(x.name, line)
-                print(x.name, line)
+    for root, _, files in os.walk('.', topdown=False):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            for accepted_file_type in accepted_file_types.keys():
+                if accepted_file_type in filename:
+                    file = FileToScrape(filename, file_path)
+                    if file.name not in sys.argv:
+                        file.read(accepted_file_type)
+                        for line in file.lines:
+                            gl.create_issue(file.name, line)
+                            print(file.name, line)
